e6aed995bb2057aff2c43cf20739a40a5ef3978d
Andrew Davis <drew@cs.purdue.edu>
2020-08-09 23:23:24 -0400

    Add Hello class

---
+++ src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * A simple class. Nothing more.
+ *
+ * @author Drew, drew@cs.purdue.edu
+ * @version Aug. 09, 2020
+ */
+public class Hello {
+
+    /**
+     * Says hello to our new friend!
+     * @param args does nothing
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!");
+    }
+
+}
